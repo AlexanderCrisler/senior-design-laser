d5a6207e28a66401c37dde29710d95ad9a6518c2
Spinver <spike9909@gmail.com>
2021-03-05 15:46:10 -0600

    Added two api calls, the 2nd (add_item) is NOT working at present. This push is so others can make modifications.

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
-from flask import Flask, render_template, flash
 import save_load
+from flask import Flask, render_template, request, make_response, jsonify
 
 app = Flask(__name__)
 start_menu = save_load.StartMenu()
 all_items = start_menu.load(file_name='master_save_file')
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+#app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+current_selection = ""
+blank_item = {'name': ''}
+
+
 
 @app.route('/')
 def hello_world():
-    return render_template('start.html', result=all_items.keys())
+    return render_template('index.html', result=all_items.keys())
 
 
 @app.route('/add')
-def adD_items():
+def add_items():
     return render_template('add_item.html')
+
+@app.route('/selected_index', methods=["POST"])
+def get_item():
+    req = request.get_json()
+    current_selection = req['name'].strip()
+    response = make_response(jsonify(all_items[current_selection]), 200)
+    return response
+
+@app.route('/add/submit', methods=['POST'])
+def submit_add_item():
+    req = request.get_json()
+    temp_dictionary = {"horizontal": req['horizontal'], "vertical": req['vertical']}
+    print(req['name'])
+    all_items[req['name']] = temp_dictionary
 
 if __name__ == '___main___':
     app.run(debug=True)
