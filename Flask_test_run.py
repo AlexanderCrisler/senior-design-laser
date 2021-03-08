52ae2acc8f7ed20fd3aac1532680d94b88332013
Jason <jason.lu.145@gmail.com>
2021-03-07 18:57:46 -0600

    Add key listeners and placeholders for laser moving functions

--- Flask_test_run.py
+++ Flask_test_run.py
@@ -1,13 +1,52 @@
-from flask import Flask, render_template
 import save_load
+from flask import Flask, render_template, request, make_response, jsonify
+
 app = Flask(__name__)
 start_menu = save_load.StartMenu()
 all_items = start_menu.load(file_name='master_save_file')
+current_selection = ""
+blank_item = {'name': ''}
+
 
 
 @app.route('/')
-def index():
-    return render_template('index.html', result=all_items)
-    
-if __name__ == '__main__':
+def hello_world():
+    return render_template('index.html', result=all_items.keys())
+
+
+@app.route('/add')
+def add_items():
+    return render_template('add_item.html')
+
+@app.route('/selected_index', methods=["POST"])
+def get_item():
+    req = request.get_json()
+    current_selection = req['name'].strip()
+    response = make_response(jsonify(all_items[current_selection]), 200)
+    return response
+
+@app.route('/add/submit', methods=['POST'])
+def submit_add_item():
+    req = request.get_json()
+    temp_dictionary = {"horizontal": req['horizontal'], "vertical": req['vertical']}
+    print(req['name'])
+    all_items[req['name']] = temp_dictionary
+
+@app.route('/key_press', methods=["POST"])
+def key_press():
+    req = request.get_json()
+
+    if (req == 39):
+        print("right key function here")
+    elif (req == 40):
+        print("down key function here")
+    elif (req == 37):
+        print("left key function here")
+    elif (req == 38):
+        print("up key function here")
+    response = req
+    return jsonify(response)
+
+
+if __name__ == '___main___':
     app.run(debug=True)
