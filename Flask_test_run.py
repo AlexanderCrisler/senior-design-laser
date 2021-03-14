e205b3afb8fd39a0c2e77836cf9b280c241ecea2
Robert Evans <hotrod1413@gmail.com>
2021-03-14 15:35:58 -0500

    Make the program run on all IP address on the computer

--- Flask_test_run.py
+++ Flask_test_run.py
@@ -3,11 +3,15 @@ import save_load
 app = Flask(__name__)
 start_menu = save_load.StartMenu()
 all_items = start_menu.load(file_name='master_save_file')
-
+#
+# This should set the server name. I had some issues getting this to work
+app.config['SERVER_NAME'] = 'localhost:5000'
 
 @app.route('/')
 def index():
     return render_template('index.html', result=all_items)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+
+#adding the host='0.0.0.0' line makes flask run on all ip addresses currently on the computer.
+    app.run(host='0.0.0.0', debug=True)
