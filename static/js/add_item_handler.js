d95cda0a82ecf19f0c92e31a0c0edcf5e49c1123
Spinver <spike9909@gmail.com>
2021-04-25 16:46:46 -0500

    Adding buttons.

--- static/js/add_item_handler.js
+++ static/js/add_item_handler.js
@@ -3,6 +3,26 @@
 
 // Window will listen for keyboard presses and call the laser moving function
 // if the right key is pressed
+function move_laser(key) {
+   console.log(move_called)
+   if (move_called == false) {
+      move_called = true
+      console.log("AA")
+      fetch(`${window.origin}/add/key_press`, {
+                  method:  "POST",
+                  credentials: "include",
+                  body: JSON.stringify(key),
+                  cache: "no-cache",
+                  headers: new Headers({ "content-type": "application/json" })
+         })
+         .then(function (response) {
+            response.json().then(function (data) {
+               setTimeout(move_called = false, 10000)
+            })
+        })
+     }
+}
+
 var move_called = false
 document.addEventListener('keydown', event => {
    console.log(move_called)
@@ -18,7 +38,7 @@ document.addEventListener('keydown', event => {
          })
          .then(function (response) {
             response.json().then(function (data) {
-               move_called = false
+               setTimeout(move_called = false, 10000)
             })
          })
       }
